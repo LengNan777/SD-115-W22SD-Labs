a11314fc37771f03f73f5e17644fa8fb8f76fd9b
LengNan777 <L2452520160@gmail.com>
2022-07-06 13:50:57 -0500

    finish lab3

---
+++ Labs/Lab3/Controllers/MVCHotelController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class MVCHotelController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Room> rooms = Hotel.Rooms;
+            return View(rooms);
+        }
+
+        public IActionResult Capacity()
+        {
+            ViewBag.Remaining = Hotel.GetVacantRooms().Count;
+            IEnumerable<Room> rooms = Hotel.Rooms;
+            return View(rooms);
+        }
+
+        public IActionResult Room(int occupants)
+        {
+            if(occupants > 6)
+            {
+                ViewBag.Message = "Sorry! There is no room available.";
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = null;
+                Room room = (from ro in Hotel.Rooms
+                             where (ro.Occupied == false & ro.Capacity >= occupants)
+                             select ro).ToList()[0];
+                return View(room);
+            }         
+        }
+    }
+}
